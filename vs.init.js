58e95a947be9f4c77950417fbd06a2a0c3e79148
Upendra Singh <UD@JADEHOL1406.local>
2017-08-14 06:49:00 +0530

    Adding an init file

---
+++ vs.init.js
@@ -0,0 +1,1 @@
+(function(w,d,s){var f=d.getElementsByTagName(s)[0], j=d.createElement(s);j.async=true;j.src= '//cdn.jsdelivr.net/gh/upendradevsingh/voice-search@v0.0.1/dist/vs.js';f.parentNode.insertBefore(j,f); })(window,document,'script');
