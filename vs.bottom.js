f3e6b47aae4231027de3ccd14ad3e08abacace63
Upendra Dev Singh <upendradevsingh@outlook.com>
2017-08-22 19:30:03 +0530

    Update vs.bottom.js

--- vs.bottom.js
+++ vs.bottom.js
@@ -39,11 +39,12 @@
 //     .vs .icon{
 //       display: inline-block;
 //       border-radius: 50%;
-//       width: 66px;
-//       height: 66px;
-//       background: -webkit-radial-gradient(#dd7e20, #ffa449);
+//       width: 60px;
+//       height: 60px;
+//       background: -webkit-radial-gradient(#ff8308,#ec9f52);
 //       color: #fff;
 //       box-shadow: 0px 0px 5px #000;
+//       padding-top: 10px;
 //     }
 //     .vs dialog .icon{animation-duration:1s;animation-iteration-count:infinite;animation-name: listening;box-shadow: 0px 0px 50px #afabab;}
 //     .vs .icon path{fill:#fff}
@@ -52,7 +53,7 @@
 //     .vs dialog::backdrop{background-color:rgba(0, 0, 0, 0.83)}
 // `;
   
-  const styles = `.vs,.vs dialog .icon{animation-duration:1s;animation-iteration-count:infinite}@keyframes listening{0%{box-shadow:0 0 50px #afabab}10%{box-shadow:0 0 45px #afabab}20%{box-shadow:0 0 43px #afabab}30%{box-shadow:0 0 40px #afabab}40%{box-shadow:0 0 30px #afabab}50%{box-shadow:0 0 27px #afabab}60%{box-shadow:0 0 25px #afabab}70%{box-shadow:0 0 20px #afabab}80%{box-shadow:0 0 15px #afabab}90%{box-shadow:0 0 10px #afabab}100%{box-shadow:0 0 0 #afabab}}.vs{position:fixed;right:10px;bottom:10px;border-radius:50%;background:#f68c24;box-sizing:border-box;text-align:center;color:#fff;border:0 solid #fff}.vs .icon{display:inline-block;border-radius:50%;width:66px;height:66px;background:-webkit-radial-gradient(#dd7e20,#ffa449);color:#fff;box-shadow:0 0 5px #000}.vs dialog .icon{animation-name:listening;box-shadow:0 0 50px #afabab}.vs .icon path{fill:#fff}.vs dialog{width:93vw;padding:50px 0;top:70px;border:transparent;background-color:#f2f2f2}.vs .vs-head{font-weight:700;font-size:1.25em}.vs dialog::backdrop{background-color:rgba(0,0,0,.83)}`;
+  const styles = `.vs,.vs dialog .icon{animation-duration:1s;animation-iteration-count:infinite}@keyframes listening{0%{box-shadow:0 0 50px #afabab}10%{box-shadow:0 0 45px #afabab}20%{box-shadow:0 0 43px #afabab}30%{box-shadow:0 0 40px #afabab}40%{box-shadow:0 0 30px #afabab}50%{box-shadow:0 0 27px #afabab}60%{box-shadow:0 0 25px #afabab}70%{box-shadow:0 0 20px #afabab}80%{box-shadow:0 0 15px #afabab}90%{box-shadow:0 0 10px #afabab}100%{box-shadow:0 0 0 #afabab}}.vs{position:fixed;right:10px;bottom:10px;border-radius:50%;background:#f68c24;box-sizing:border-box;text-align:center;color:#fff;border:0 solid #fff}.vs .icon{display:inline-block;border-radius:50%;width:60px;height:60px;background:-webkit-radial-gradient(#ff8308,#ec9f52);color:#fff;box-shadow:0 0 5px #000;padding-top:10px}.vs dialog .icon{animation-name:listening;box-shadow:0 0 50px #afabab}.vs .icon path{fill:#fff}.vs dialog{width:93vw;padding:50px 0;top:70px;border:transparent;background-color:#f2f2f2}.vs .vs-head{font-weight:700;font-size:1.25em}.vs dialog::backdrop{background-color:rgba(0,0,0,.83)}`;
 
   const onspeechend = function() {
     Recognition.mic.style.animationName = 'none';
@@ -98,7 +99,7 @@
       default: {
         icon = document.createElement('i');
         icon.classList.add('icon');
-        icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="54" height="58" viewBox="0 -4 24 24"><path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z"></path></svg>'
+        icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24"><path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z"></path></svg>'
       }
     }
     return icon;
